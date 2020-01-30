fc31d39d236a04dbef87ecbc401e39dd67c1e967
duorourou <duoorourou@gmail.com>
2020-01-30 17:36:57 +0800

    [duorourou] #Leetcode-interval-list-intersections  skip this case

---
+++ interval-list-intersections/Solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+#TODO skip
+class Solution:
+	def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+		return A
+
+
+if __name__ == '__main__':
+	print(Solution().intervalIntersection([[0, 2], [5, 10], [13, 23], [24, 25]], [[1, 5], [8, 12], [15, 24], [25, 26]]))
